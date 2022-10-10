38cc0f561599259c9e5c65c4e0f112e83620884f
elizalap <elizapaoliello@outlook.com>
2022-10-10 18:10:54 -0300

    Método nomeCompleto e concatenação

--- aprendendo-sintaxe-java/java-anatomia-classes/src/MinhaClasse.java
+++ aprendendo-sintaxe-java/java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,16 @@
 public class MinhaClasse {
 
    public static void main (String [] args) {
-      System.out.print("Hello, world!");
+      
+      String primeiroNome = "Eliza";
+      String segundoNome = "Lima";
+
+      String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+      System.out.print(nomeCompleto);
    }
+}
+
+public static String nomeCompleto(String primeiroNome, String segundoNome) {
+   return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
 }
